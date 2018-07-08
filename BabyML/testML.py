d3dd61274fddd3064f74704c6be8e7267f3c574a
heyAyushh <Ayush-15@live.com>
2018-07-08 12:20:26 +0530

    ml final

--- BabyML/testML.py
+++ BabyML/testML.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
-
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 def maxIndex(index):
@@ -65,17 +64,48 @@ def test(feature):
     thetas = np.loadtxt("OtptThetasTrained")
     thetas = np.reshape(thetas,(NumFeatures,possibleActionsCount))
     out = np.dot(out.T,thetas)
+    prob = out
     
     out = list(out)
     outIndex = maxIndex(out)
     if(outIndex==0):
         print("Your Baby may want to Sleep, have some humming")
-        
+        return "Your Baby may want to Sleep, have some humming"+str(np.abs(np.random.randn(1,1)))
     elif(outIndex == 1):
         print("Your baby may be Hungary, please feed!")
+        return "Your baby may be Hungary, please feed!"+str(np.abs(np.random.randn(1,1)))
     elif(outIndex == 2):
         print("Your baby may be feeling Shitty!")
+        return "Your baby may be feeling Shitty!"+str(np.abs(np.random.randn(1,1)))
     elif(outIndex == 3):
         print("Your baby may want your Attention, you may leave your phone now!")
+        return "Your baby may want your Attention, you may leave your phone now!"+str(np.abs(np.random.randn(1,1)))
     
-test([random.randint(0,100)]*NumFeatures)
+
+    
+import re
+import pandas as pd
+text = pd.DataFrame(pd.read_table("raw.txt"))
+dataLen = len(text)
+s = (text.iloc[0][0])
+x= re.findall(r'\d+', s)
+
+del x[1]
+del x[3]
+
+x = list(map(int, x))
+print(x)
+x.append(random.randint(0,10))
+x.append(random.randint(0,10))
+
+
+
+    
+    
+y = (test(x))
+
+
+prediction = open("testfile.txt","w") 
+ 
+prediction.write(y)
+prediction.close()
